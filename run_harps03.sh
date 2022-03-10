43c7999d9d29f829958afb21530839d3ead9d240
Denis Rosset <physics@denisrosset.com>
2022-03-10 15:29:19 +0100

    Renamed RASSINE steps

--- run_harps03.sh
+++ run_harps03.sh
@@ -32,9 +32,9 @@ matching_parameters=/home/denis/w/rassine1/spectra_library/HD23249/data/s1d/HARP
 preprocessed_dir=/home/denis/w/rassine1/spectra_library/HD23249/data/s1d/HARPS03/PREPROCESSED
 
 # TODO: -> rassine_borders_scan.py
-# TODO: -> rassine_bordaers_reinterpolate.py
+# TODO: -> rassine_borders_reinterpolate.py
 
-python rassine_preprocess_match_stellar_frame.py --input-dir "$preprocessed_dir" --dlambda 0.01 -k "$dace_table" --output-file "$matching_parameters"
+python rassine_borders_scan.py --input-dir "$preprocessed_dir" --dlambda 0.01 -k "$dace_table" --output-file "$matching_parameters"
 
 # Step 2B we process each spectrum in parallel
 # files are overwritten
@@ -43,7 +43,7 @@ nchunks_matching=10
 nthreads_matching=4
 
 nspectra=$(ls "$preprocessed_dir"/*.p | wc -l)
-seq 0 $(($nspectra - 1)) | parallel -N$nchunks_matching --jobs $nthreads_matching --keep-order python rassine_matching.py --parameter-file "$matching_parameters" --input-dir "$preprocessed_dir"
+seq 0 $(($nspectra - 1)) | parallel -N$nchunks_matching --jobs $nthreads_matching --keep-order python rassine_borders_reinterpolate.py --parameter-file "$matching_parameters" --input-dir "$preprocessed_dir"
 
 ## Stacking
 # Step 3
@@ -51,7 +51,6 @@ seq 0 $(($nspectra - 1)) | parallel -N$nchunks_matching --jobs $nthreads_matchin
 # this creates the MASTER file which sums all the spectra
 # The output files are written in /STACKED
 mkdir -p /home/denis/w/rassine1/spectra_library/HD23249/data/s1d/HARPS03/MASTER
-# TODO: name ok
 python rassine_stacking.py --input_directory /home/denis/w/rassine1/spectra_library/HD23249/data/s1d/HARPS03/PREPROCESSED
 
 # get the latest master file
@@ -82,37 +81,32 @@ ls /home/denis/w/rassine1/spectra_library/HD23249/data/s1d/HARPS03/STACKED/Stack
 ## Intersect continuum
 # This was python Rassine_multiprocessed.py -v INTERSECT -s /home/denis/w/rassine1/spectra_library/HD23249/data/s1d/HARPS03/STACKED/RASSINE -n $nthreads_intersect
 
-# TODO: rassine_anchor_scan.py
-# TODO: rassine_anchor_filter.py
 # Step 5A: computation of the parameters
 # See Fig D7
 nthreads_intersect1=4
-python rassine_intersect1.py --input-directory /home/denis/w/rassine1/spectra_library/HD23249/data/s1d/HARPS03/STACKED --feedback $not_full_auto --no-master-spectrum --copies-master 0 --kind anchor_index --nthreads $nthreads_intersect1 --fraction 0.2 --threshold 0.66 --tolerance 0.5 --add-new True
+python rassine_anchor_scan.py --input-directory /home/denis/w/rassine1/spectra_library/HD23249/data/s1d/HARPS03/STACKED --feedback $not_full_auto --no-master-spectrum --copies-master 0 --kind anchor_index --nthreads $nthreads_intersect1 --fraction 0.2 --threshold 0.66 --tolerance 0.5 --add-new True
 
 # Step 5B: application in parallel
 # Done in place
-# "matching_anchor"
 nthreads_intersect2=4
 nchunks_intersect2=10
 
-ls /home/denis/w/rassine1/spectra_library/HD23249/data/s1d/HARPS03/STACKED/RASSINE* | parallel -N$nchunks_intersect2 --jobs $nthreads_intersect2 --keep-order python rassine_intersect2.py
+ls /home/denis/w/rassine1/spectra_library/HD23249/data/s1d/HARPS03/STACKED/RASSINE* | parallel -N$nchunks_intersect2 --jobs $nthreads_intersect2 --keep-order python rassine_anchor_filter.py
 # note: removed the master spectrum option
 
 ## matching_diff_continuum_sphinx
 
-# TODO: rassine_savgol_gui.py
-# TODO: rassine_savgol_filter.py
 # Step 6A
 # See Fig D8 of the RASSINE paper
 output_savgol_window=/home/denis/w/rassine1/spectra_library/HD23249/data/s1d/HARPS03/savgol_window.txt
-python rassine_matching_diff_continuum_sphinx.py --input_directory /home/denis/w/rassine1/spectra_library/HD23249/data/s1d/HARPS03/STACKED --output-file $output_savgol_window --no-master
+python rassine_savgol_gui.py --input_directory /home/denis/w/rassine1/spectra_library/HD23249/data/s1d/HARPS03/STACKED --output-file $output_savgol_window --no-master
 savgol_window=$(< $output_savgol_window)
 
 # Step 6B done in parallel
 # "matching_diff"
 nthreads_savgol=4
 nchunks_savgol=10
-ls /home/denis/w/rassine1/spectra_library/HD23249/data/s1d/HARPS03/STACKED/RASSINE* | parallel -N$nchunks_savgol --jobs $nthreads_savgol --keep-order python rassine_savgol.py --anchor-file $rassine_master --savgol-window $savgol_window
+ls /home/denis/w/rassine1/spectra_library/HD23249/data/s1d/HARPS03/STACKED/RASSINE* | parallel -N$nchunks_savgol --jobs $nthreads_savgol --keep-order python rassine_savgol_filter.py --anchor-file $rassine_master --savgol-window $savgol_window
 #python Rassine_multiprocessed.py -v SAVGOL -s /home/denis/w/rassine1/spectra_library/HD23249/data/s1d/HARPS03/STACKED/RASSINE -n $nthreads_intersect -l $rassine_master -P True -e False -w $savgol_window
 touch /home/denis/w/rassine1/spectra_library/HD23249/data/s1d/HARPS03/rassine_finished.txt
 
